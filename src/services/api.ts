8abd87e44b4ab81029219196f9a863daaa984f92
Sergey Kabanets <skabanets97@gmail.com>
2024-09-03 16:08:03 +0300

    Add try-catch for requests

--- src/services/api.ts
+++ src/services/api.ts
@@ -9,10 +9,36 @@ export const api = axios.create({
 });
 
 export const getAllBooks = async (query: string = ""): Promise<Book[]> => {
-  const { data } = await api.get(`/books?query=${query}`);
-  return data;
+  try {
+    const { data } = await api.get(`/books?query=${query}`);
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(error.response?.data.message || "An error occurred");
+    }
+    throw new Error("An unknown error occurred");
+  }
+};
+
+export const addBook = async (book: Omit<Book, "_id">): Promise<Book> => {
+  try {
+    const newBook = await api.post(`/books`, book);
+    return newBook.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(error.response?.data.message || "An error occurred");
+    }
+    throw new Error("An unknown error occurred");
+  }
 };
 
 export const deleteBookByIsbn = async (isbn: string): Promise<void> => {
-  await api.delete(`/books/${isbn}`);
+  try {
+    await api.delete(`/books/${isbn}`);
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(error.response?.data.message || "An error occurred");
+    }
+    throw new Error("An unknown error occurred");
+  }
 };

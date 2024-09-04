f1137066d24b4a2ae03261e0671cc27179739af8
Sergey Kabanets <skabanets97@gmail.com>
2024-09-04 21:55:45 +0300

    Add modern-normalize

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { ToastContainer } from "react-toastify";
 import { App } from "./components/App";
 
 import "./index.css";
+import "modern-normalize/modern-normalize.css";
 import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
